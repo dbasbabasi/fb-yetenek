6d53b4f23819828ce5d2e6a92f3abcc66e3f032c
Dervis Basbabasi <dervis.basbabasi@softtech.com.tr>
2018-03-05 11:52:29 +0300

    try

--- controllers/test.js
+++ controllers/test.js
@@ -20,25 +20,23 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 router.get('/', function (request, response, next) {
-
-
-bot.on('error', (err) => {
-  console.log(err.message)
-})
-
-bot.on('message', (payload, reply) => {
-    let text = payload.message.text
-   
-    bot.getProfile(payload.sender.id, (err, profile) => {
-      if (err) throw err
-   
-      reply({ text }, (err) => {
-        if (err) throw err
-   
-        console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-      })
+    bot.on('error', (err) => {
+    console.log(err.message)
     })
-  })
+
+    bot.on('message', (payload, reply) => {
+        let text = payload.message.text
+    
+        bot.getProfile(payload.sender.id, (err, profile) => {
+        if (err) throw err
+    
+        reply({ text }, (err) => {
+            if (err) throw err
+    
+            console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+        })
+        })
+    })
 });
 
 module.exports = router;
