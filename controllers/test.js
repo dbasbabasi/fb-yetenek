e79d4817028a2750863deb90d6ff00a6f6593ecb
Dervis Basbabasi <dervis.basbabasi@softtech.com.tr>
2018-03-05 11:45:18 +0300

    try

--- controllers/test.js
+++ controllers/test.js
@@ -7,6 +7,10 @@ var config = require('config/config.json');
 const FB_TOKEN = 'EAACenlCJHQIBAGTH1R7ZB9qW08bbbSByS1IWNZBE7xIJ4TICP64kQckTctT4qdd3h9M8uHhBAzBxhs0RnmZB1pTNtscgmdaYa3AZBAsEVBciDQKDM8TOj56O9wxvJC9CXm1g4CzKEY7U65MUXbIXgFP8Vt178zAZAdejZBMPzkYwZDZD'
 const FB_VERIFY = 'Softtech2018'
 
+let bot = new Bot({
+  token: FB_TOKEN,
+  verify: FB_VERIFY
+})
 
 //body parser intialize
 var bodyParser = require('body-parser');
@@ -17,10 +21,6 @@ router.use(bodyParser.json());
 
 router.get('/', function (request, response, next) {
 
-let bot = new Bot({
-  token: FB_TOKEN,
-  verify: FB_VERIFY
-})
 
 bot.on('error', (err) => {
   console.log(err.message)
