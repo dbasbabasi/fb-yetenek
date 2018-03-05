b1e9eae6d487e394c0baa171d793aeac78453a1b
Dervis Basbabasi <dervis.basbabasi@softtech.com.tr>
2018-03-05 14:09:56 +0300

    setGetStartedButton

--- server.js
+++ server.js
@@ -27,17 +27,21 @@ bot.on('error', (err) => {
 console.log(err.message)
 })
 
+bot.setGetStartedButton((payload, callback) => {
+    reply({ text: 'Merhaba Softtech Yetenek Kuşağına Hoşgeldiniz.' })
+
+
+})
+
 bot.on('message', (payload, reply) => {
     let text = payload.message.text
-
     bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) throw err
-
-    reply({ text }, (err) => {
         if (err) throw err
 
-        console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
-    })
+        reply({ text }, (err) => {
+            if (err) throw err
+            console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+        })
     })
 })
 
